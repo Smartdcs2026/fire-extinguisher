32124e6fbccfd4246d0a4da9fe22cba83ee720c2
SKDC_SMART <somchaibutphon@gmail.com>
2026-05-31 11:50:52 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,27 @@
+// (function () {
+//   'use strict';
+
+//   window.APP_CONFIG = {
+//     APP_NAME: 'Fire Extinguisher',
+//     APP_TITLE: 'ระบบตรวจเช็คถังดับเพลิง',
+//     API_BASE: 'https://fireextinguisher.somchaibutphon.workers.dev',
+//     GITHUB_BASE: 'https://smartdcs2026.github.io/fire-extinguisher',
+//     DEFAULT_PERIOD: 'thisMonth'
+//   };
+// })();
 (function () {
   'use strict';
+
+  const LOGO_FILE_ID = '1HicYHV18UaA5y4GFyHJaG9aNI-qjIzIY';
 
   window.APP_CONFIG = {
     APP_NAME: 'Fire Extinguisher',
     APP_TITLE: 'ระบบตรวจเช็คถังดับเพลิง',
     API_BASE: 'https://fireextinguisher.somchaibutphon.workers.dev',
     GITHUB_BASE: 'https://smartdcs2026.github.io/fire-extinguisher',
-    DEFAULT_PERIOD: 'thisMonth'
+    DEFAULT_PERIOD: 'thisMonth',
+
+    LOGO_FILE_ID,
+    LOGO_URL: `https://drive.google.com/thumbnail?id=${LOGO_FILE_ID}&sz=w300`
   };
 })();
