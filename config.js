b979daddf154b7930fe58f79362615cfd14b0929
SKDC_SMART <somchaibutphon@gmail.com>
2026-05-31 10:36:00 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  window.APP_CONFIG = {
+    APP_NAME: 'Fire Extinguisher',
+    APP_TITLE: 'ระบบตรวจเช็คถังดับเพลิง',
+    API_BASE: 'https://fireextinguisher.somchaibutphon.workers.dev',
+    GITHUB_BASE: 'https://somchaibutphon.github.io/fire-extinguisher',
+    DEFAULT_PERIOD: 'thisMonth'
+  };
+})();
