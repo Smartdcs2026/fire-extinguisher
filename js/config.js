09f65d7a59ad45c15467ca3bb453311325ed52b8
SKDC_SMART <somchaibutphon@gmail.com>
2026-06-02 07:34:40 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,6 +4,7 @@
   const LOGO_FILE_ID = '1HicYHV18UaA5y4GFyHJaG9aNI-qjIzIY';
 
   function driveImage(fileId) {
+    if (!fileId || String(fileId).includes('ใส่_ID')) return '';
     return `https://lh5.googleusercontent.com/d/${fileId}`;
   }
 
@@ -18,7 +19,7 @@
     LOGO_URL: driveImage(LOGO_FILE_ID),
 
     EXTINGUISHER_IMAGES: {
-      DEFAULT: driveImage('ใส่_ID_ภาพถังทั่วไป'),
+      DEFAULT: '',
 
       CO2: driveImage('1_f-EOMtCHFzT9ycVqsprJwzlceKu-uwt'),
       DRY_CHEMICAL: driveImage('1MnL6ozRx2mrVAd490UZF84dN5lBJhBeZ'),
@@ -27,48 +28,4 @@
       LOW_PRESSURE_WATER: driveImage('1th0gYk5GU7S7YgEj4XzgOZH1bpKTDrYx')
     }
   };
-  function getExtinguisherImage(typeText) {
-  const text = String(typeText || '').toLowerCase();
-  const images = window.APP_CONFIG.EXTINGUISHER_IMAGES || {};
-
-  if (
-    text.includes('co2') ||
-    text.includes('carbon dioxide') ||
-    text.includes('คาร์บอนไดออกไซด์')
-  ) {
-    return images.CO2 || images.DEFAULT || '';
-  }
-
-  if (
-    text.includes('dry chemical') ||
-    text.includes('ผงเคมี') ||
-    text.includes('เคมีแห้ง')
-  ) {
-    return images.DRY_CHEMICAL || images.DEFAULT || '';
-  }
-
-  if (
-    text.includes('foam') ||
-    text.includes('โฟม')
-  ) {
-    return images.FOAM || images.DEFAULT || '';
-  }
-
-  if (
-    text.includes('low pressure water') ||
-    text.includes('low pressure') ||
-    text.includes('water vapor')
-  ) {
-    return images.LOW_PRESSURE_WATER || images.WATER || images.DEFAULT || '';
-  }
-
-  if (
-    text.includes('water') ||
-    text.includes('น้ำ')
-  ) {
-    return images.WATER || images.DEFAULT || '';
-  }
-
-  return images.DEFAULT || '';
-}
 })();
